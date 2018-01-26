a1a939be61d020fbf72c2b01323db8a04ec57ed5
Erikson Kaszubowski <erikson84@yahoo.com.br>
2018-01-26 15:56:01 -0200

    Forgot example code in lavaanStan source.

--- lavaanStan.R
+++ lavaanStan.R
@@ -35,22 +35,3 @@ lavaanStan <- function(
                   init=lapply(1:chains, function(x) initf(fit)))
   stanFit
 }
-
-data("PoliticalDemocracy")
-dados <- PoliticalDemocracy
-model <- '
-   # latent variables
-     ind60 =~ x1 + x2 + x3
-     dem60 =~ y1 + y2 + y3 + y4
-     dem65 =~ y5 + y6 + y7 + y8
-   # regressions
-     dem60 ~ ind60
-     dem65 ~ ind60 + dem60
-   # residual covariances
-     y1 ~~ y5
-     y2 ~~ y4 + y6
-     y3 ~~ y7
-     y4 ~~ y8
-     y6 ~~ y8
-'
-fit <- lavaanStan(model=model, data=dados, model.type='sem')
